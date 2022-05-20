dae1468f9393639b42cc0273a23708301847144d
kallif003 <66094168+kallif003@users.noreply.github.com>
2022-05-20 12:17:51 -0300

    feat: :heavy_plus_sign: install prettier and eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import React from 'react' 
-import type { NextPage } from 'next'
+import React from "react"
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return (
-    <div >
-      
-    </div>
-  )
+	return <div> oi</div>
 }
 
 export default Home
