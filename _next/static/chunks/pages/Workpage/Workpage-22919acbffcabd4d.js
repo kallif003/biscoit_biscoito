39edcdfb52492795de87482dbc828800ee70a21c
kallif003 <kallif003@users.noreply.github.com>
2022-06-06 16:21:22 +0000

    Deploying to gh-pages from @ kallif003/biscuit-biscoito.github.io@27f5b2c0010ba6bf2d3b0845ac484df6a264692f 🚀

---
+++ _next/static/chunks/pages/Workpage/Workpage-22919acbffcabd4d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[911],{9112:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Workpage/Workpage",function(){return u(1625)}])}},function(n){n.O(0,[702,151,625,774,888,179],(function(){return _=9112,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
