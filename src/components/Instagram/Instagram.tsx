e671592dfb66b760ae42c43edc423445910bf8c3
kallif003 <kallifabrahao@gmail.com>
2022-06-07 10:22:43 -0300

    feat: :sparkles: new changes to the Instagram carousel

--- src/components/Instagram/Instagram.tsx
+++ src/components/Instagram/Instagram.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { NextPage } from "next"
 import Carousel from "react-elastic-carousel"
-import me from "../../assets/me.png"
-import Image from "next/image"
 import { ContainerInstragram, DivCarousel, DivImg } from "."
 
 const Instagram: NextPage = () => {
@@ -10,15 +8,9 @@ const Instagram: NextPage = () => {
 		<ContainerInstragram>
 			<DivCarousel>
 				<Carousel itemPadding={[50, 0]}>
-					<DivImg className="rotate-[20deg]">
-						<Image src={me} alt="me" width={500} height={600} />
-					</DivImg>
-					<DivImg>
-						<Image src={me} alt="me" width={500} height={600} />
-					</DivImg>
-					<DivImg>
-						<Image src={me} alt="me" width={500} height={600} />
-					</DivImg>
+					<DivImg className="rotate-[20deg]"></DivImg>
+					<DivImg></DivImg>
+					<DivImg></DivImg>
 				</Carousel>
 			</DivCarousel>
 		</ContainerInstragram>
